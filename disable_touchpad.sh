5afe3c26a30a632e1041f2d31d9e8d29626559bc
Kaveh Shahhosseini <kaveh228@gmail.com>
2023-09-05 17:25:03 +0100

    Update disable_touchpad.sh

--- disable_touchpad.sh
+++ disable_touchpad.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-mouse=`xinput | grep 'Mouse' | cut -d$'\t' -f2`
-touch=`xinput | grep 'Touchpad' | cut -d$'\t' -f2`
+mouse=$(xinput | grep 'Mouse' | grep -v 'Optical' | cut -f2)
+touchpad=$(xinput | grep 'Touchpad' | cut -f2)
 
-echo 'Mouse' $mouse
-echo 'Touch' $touch
+disable_mousepad() {
+    echo 'Disabling Mouse...' $mouse
+    mouse_id=$(echo $mouse | cut -d '=' -f 2)
+    mouse_prop_id=$(xinput list-props $mouse_id | grep 'Device Enabled' | cut -d '(' -f 2 | cut -d ')' -f 1)
+    echo 'Mouse'$(xinput list-props $mouse_id | grep 'Device Enabled')
+    xinput set-prop $mouse_id $mouse_prop_id 0
+    echo 'Mouse'$(xinput list-props $mouse_id | grep 'Device Enabled')
+    echo 'Mouse Disabled.'
+}
 
-mouse_id=`echo $mouse | cut -d '=' -f 2`
-touch_id=`echo $touch | cut -d '=' -f 2`
+disable_touchpad() {
+    echo 'Disabling Touch...' $touchpad
+    touchpad_id=$(echo $touchpad | cut -d '=' -f 2)
+    touchpad_prop_id=$(xinput list-props $touchpad_id | grep 'Device Enabled' | cut -d '(' -f 2 | cut -d ')' -f 1)
+    echo 'Touch'$(xinput list-props $touchpad_id | grep 'Device Enabled')
+    xinput set-prop $touchpad_id $touchpad_prop_id 0
+    echo 'Touch'$(xinput list-props $touchpad_id | grep 'Device Enabled')
+    echo 'Touch Disabled.'
+}
 
-mouse_prop_id=`xinput list-props $mouse_id | grep 'Device Enabled' | cut -d '(' -f 2 | cut -d ')' -f 1`
-touch_prop_id=`xinput list-props $touch_id | grep 'Device Enabled' | cut -d '(' -f 2 | cut -d ')' -f 1`
-
-echo
-echo 'Mouse'`xinput list-props $mouse_id | grep 'Device Enabled'`
-echo 'Touch'`xinput list-props $touch_id | grep 'Device Enabled'`
-
-echo 
-xinput set-prop $mouse_id $mouse_prop_id 0
-xinput set-prop $touch_id $touch_prop_id 0
-
-echo '*****************************************'
-
-echo
-echo 'Mouse'`xinput list-props $mouse_id | grep 'Device Enabled'`
-echo 'Touch'`xinput list-props $touch_id | grep 'Device Enabled'`
+if [ -z "$mouse" ] && [ -z "$touchpad" ]; then
+    echo "No Mousepad/Touchpad Detected."
+    set -e
+elif [ -z "$mouse" ] && [ ! -z "$touchpad" ]; then
+    echo "No Mousepad Detected."
+    disable_touchpad
+elif [ -z "$touchpad" ] && [ ! -z "$mouse" ]; then
+    echo "No Touchpad Detected."
+    disable_mousepad
+else
+    disable_mousepad
+    disable_touchpad
+fi
